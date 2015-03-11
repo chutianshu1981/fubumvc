b3e1205863f42b8890ed826952d7bc9cd720fb07
jeremydmiller <jeremydmiller@yahoo.com>
2015-03-11 10:53:20 -0500

    new aggregator service in action

--- src/FubuMVC.Tests/Registration/CoreServiceRegistry_specification.cs
+++ src/FubuMVC.Tests/Registration/CoreServiceRegistry_specification.cs
@@ -14,6 +14,7 @@ using FubuMVC.Core.Http.Cookies;
 using FubuMVC.Core.Registration;
 using FubuMVC.Core.Registration.Querying;
 using FubuMVC.Core.Runtime;
+using FubuMVC.Core.Runtime.Aggregation;
 using FubuMVC.Core.Runtime.Conditionals;
 using FubuMVC.Core.Runtime.Files;
 using FubuMVC.Core.SessionState;
@@ -39,6 +40,12 @@ namespace FubuMVC.Tests.Registration
         {
             _serviceGraph.DefaultServiceFor<TService>().Type.ShouldEqual(
                 typeof (TImplementation));
+        }
+
+        [Test]
+        public void aggregator_is_registered()
+        {
+            registeredTypeIs<IAggregator, Aggregator>();
         }
 
         [Test]
